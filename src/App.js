f31971964e9cbc909e0a94fdf7ecc0e466c0e805
maxprad <zeeschedule@gmail.com>
2021-04-26 13:48:21 -0400

    uses just MUI AppBar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <AppBar color="primary" position="static">
+          <h1>My header</h1>
+        </AppBar>
+
+      </div>
+    );
+  }
 }
-
 export default App;
