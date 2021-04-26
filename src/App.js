28091a05b5ac0785362514439bda4d615b7dccdf
maxprad <zeeschedule@gmail.com>
2021-04-26 15:48:37 -0400

    Added an white cloud image

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React, { Component } from 'react';
-import AppBar from '@material-ui/core/AppBar';
+import React from "react";
+import ReactDOM from "react-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from '@material-ui/core/styles';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <AppBar color="primary" position="static">
-          <h1>My header</h1>
-        </AppBar>
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minHeight: '100vh',
+    backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/P1090708_bg.jpg'})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+  },
+}));
 
-      </div>
-    );
-  }
-}
+function App() {
+const classes = useStyles();
+  return (
+    <div className={classes.root}>
+Home Page
+ </div>
+  );
+}      
 export default App;
